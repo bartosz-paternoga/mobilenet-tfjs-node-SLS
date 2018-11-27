a6d859e0d2466867299b61f0022cbadc92fb5168
bartosz-paternoga <bpaternoga@hotmail.com>
2018-11-27 16:47:37 +0100

    Updates

---
+++ precache-manifest.6082e3f6aa96fa9686f490184c024aee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "77b415b47e0277aea2bc",
+    "url": "/mobilenet-tfjs-node-sls/static/css/main.64f716fc.chunk.css"
+  },
+  {
+    "revision": "77b415b47e0277aea2bc",
+    "url": "/mobilenet-tfjs-node-sls/static/js/main.77b415b4.chunk.js"
+  },
+  {
+    "revision": "de741bea41e3a72aeb34",
+    "url": "/mobilenet-tfjs-node-sls/static/js/1.de741bea.chunk.js"
+  },
+  {
+    "revision": "4644200a44941c093221",
+    "url": "/mobilenet-tfjs-node-sls/static/js/runtime~main.4644200a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mobilenet-tfjs-node-sls/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d3b3ccc5c26caf00f4644c10d6c92ce8",
+    "url": "/mobilenet-tfjs-node-sls/static/media/sound.d3b3ccc5.mp3"
+  },
+  {
+    "revision": "aef83ebefa49b72255a9248181699d91",
+    "url": "/mobilenet-tfjs-node-sls/index.html"
+  }
+];
